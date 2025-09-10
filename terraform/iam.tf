9c012c9af083fb3ab5caaef2b9ddd60d89537bf6
Vin22-03 <vinay.venvin@gmail.com>
2025-09-10 14:01:45 +0000

    changed infra

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,4 +1,6 @@
+# ----------------------------
 # EKS Cluster Role
+# ----------------------------
 resource "aws_iam_role" "eks_cluster_role" {
   name = "${var.project_name}-eks-cluster-role"
 
@@ -27,8 +29,9 @@ resource "aws_iam_role_policy_attachment" "eks_cluster_attach" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
-
+# ----------------------------
 # EKS Node Role
+# ----------------------------
 resource "aws_iam_role" "eks_node_role" {
   name = "${var.project_name}-eks-node-role"
 
@@ -52,76 +55,6 @@ resource "aws_iam_role" "eks_node_role" {
   }
 }
 
-# Attach required policies to node role
-resource "aws_iam_role_policy_attachment" "eks_worker_attach1" {
-  role       = aws_iam_role.eks_node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-}
-
-resource "aws_iam_role_policy_attachment" "eks_worker_attach2" {
-  role       = aws_iam_role.eks_node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-}
-
-resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
-  role       = aws_iam_role.eks_node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-}
-# EKS Cluster Role
-resource "aws_iam_role" "eks_cluster_role" {
-  name = "${var.project_name}-eks-cluster-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "eks.amazonaws.com"
-      }
-    }]
-  })
-
-  lifecycle {
-    prevent_destroy = true
-  }
-
-  tags = {
-    Name = "${var.project_name}-eks-cluster-role"
-  }
-}
-
-resource "aws_iam_role_policy_attachment" "eks_cluster_attach" {
-  role       = aws_iam_role.eks_cluster_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-}
-
-
-# EKS Node Role
-resource "aws_iam_role" "eks_node_role" {
-  name = "${var.project_name}-eks-node-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
-    }]
-  })
-
-  lifecycle {
-    prevent_destroy = true
-  }
-
-  tags = {
-    Name = "${var.project_name}-eks-node-role"
-  }
-}
-
-# Attach required policies to node role
 resource "aws_iam_role_policy_attachment" "eks_worker_attach1" {
   role       = aws_iam_role.eks_node_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
