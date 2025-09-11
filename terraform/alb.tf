fa19f9baa83253bdf34bc06c7de4a2109b9251f8
Vin22-03 <vinay.venvin@gmail.com>
2025-09-11 07:15:42 +0000

    updated

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -23,13 +23,13 @@ resource "aws_lb_target_group" "frontend_tg" {
   }
 }
 
-resource "aws_lb_listener" "frontend_listener" {
-  load_balancer_arn = aws_lb.frontend_alb.arn
-  port              = 80
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.frontend_tg.arn
-  }
-}
+#resource "aws_lb_listener" "frontend_listener" {
+ # load_balancer_arn = aws_lb.frontend_alb.arn
+  #port              = 80
+  #protocol          = "HTTP"
+#
+ # default_action {
+  #  type             = "forward"
+   # target_group_arn = aws_lb_target_group.frontend_tg.arn
+  #}
+#}
