097dbd42858ad991e9ba6707071d491743ce469c
Vin22-03 <vinay.venvin@gmail.com>
2025-09-11 10:32:29 +0000

    Added frontend deployment to pipeline

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -8,7 +8,7 @@ resource "aws_lb" "frontend_alb" {
 
 resource "aws_lb_target_group" "frontend_tg" {
   name     = "frontend-tg"
-  port     = 3001
+  port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id
   target_type = "ip"
@@ -28,8 +28,8 @@ resource "aws_lb_listener" "frontend_listener" {
   port              = 80
   protocol          = "HTTP"
 
-  default_action {
-    type             = "forward"
+ default_action {
+   type             = "forward"
     target_group_arn = aws_lb_target_group.frontend_tg.arn
   }
 }
